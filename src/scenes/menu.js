9db9fc0acff24857fc3872aa42cff82d56a6d501
Seth-Hahn <sethehahn2004@gmail.com>
2025-03-10 18:50:40 -0700

    added exit door and resized sprite sheets

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -7,19 +7,29 @@ class Menu extends Phaser.Scene {
 
         //load images and sprites
         this.load.image('sky', './assets/img/Xtreme_Xcape_Background.png')
+        this.load.image('exit', './assets/img/Exit_Door.png')
         this.load.spritesheet('P1', './assets/img/P1_SpriteSheet.png', {
-            frameWidth:90,
-            frameHeight:90,
+            frameWidth:120,
+            frameHeight:120,
         })
         this.load.spritesheet('P2', './assets/img/P2_SpriteSheet.png', {
-            frameWidth:90,
-            frameHeight:90,
+            frameWidth:120,
+            frameHeight:120,
         })
+
+    
 
     }
 
     create() {
-        
+        //initialize each players score
+        this.registry.set('P1_Score', 0)
+        this.registry.set('P2_Score', 0)
+
+        //initialize bool which allows one scene 
+        //to switch to the next after player reaches exit
+        this.registry.set('ExitReached', false)
+
         console.log('menu scene loaded')
         this.scene.start('round1Scene')
     }
