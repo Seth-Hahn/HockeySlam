e20caf243c9583956bef4083189d74d2f70aafd5
Seth-Hahn <sethehahn2004@gmail.com>
2025-03-21 20:42:56 -0700

    end screen updated

--- src/scenes/end.js
+++ src/scenes/end.js
@@ -19,8 +19,21 @@ class End extends Phaser.Scene {
         this.winText = data.winText
     }
     create() {
+        this.winningPlayer = this.add.sprite(game.config.width / 1.1 , game.config.height / 1.3, this.winner, 0).setDepth(999).setScale(2,2)
         this.background = this.add.sprite(0,0, 'endScreen').setOrigin(0,0)
-        this.winTextDisplay = this.add.text(game.config.height / 2, game.config.width / 2, this.winText, this.endTextConfig)
+        this.winTextDisplay = this.add.text(game.config.width / 2, game.config.height / 5, this.winText, this.endTextConfig)
+        this.emitter = this.add.particles(game.config.width / 2.6 , game.config.height / 1.3 , 'particle', {
+            speed: { min: -300, max: 300 },
+            angle: {min: 0, max: 360 },
+            scale: { start: 0.25, end: 0},
+            lifespan: 300,
+            quantity: 100,
+            gravityY: 100,
+            emitting: true,
+        }).setScale(5,5)
+
+        this.menuButton = this.add.text(game.config.width / 20, game.config.height / 3.5 , 'MENU', this.endTextConfig)
+        this.restartButton = this.add.text(game.config.width / 20, game.config.height / 3, 'RESTART', this.endTextConfig)
     }
     
 }
--- src/scenes/round2.js
+++ src/scenes/round2.js
@@ -16,9 +16,9 @@ class Round2 extends Gameround {
             let p1Score = this.registry.get('P1_Score')
             let p2Score = this.registry.get('P2_Score')
             if(p1Score >= 2) {
-                this.endGame({winner: this.P1, winText: "Player 1 Wins"})
+                this.endGame({winner: 'P1', winText: "Player 1 Wins"})
             } else if (p2Score >= 2) {
-                this.endGame({winner: this.P2, winText: "Player 2 Wins"})
+                this.endGame({winner: 'P2', winText: "Player 2 Wins"})
             } else {
                 this.scene.start('round3Scene')
             }
